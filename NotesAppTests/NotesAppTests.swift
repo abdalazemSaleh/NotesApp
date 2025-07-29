934a3be8f9b93796627085392de615dc5c51ab93
abdalazemsaleh <abdalazem.saleh2@icloud.com>
2025-07-29 22:41:07 +0300

    Initial Commit

---
+++ NotesAppTests/NotesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppTests.swift
+//  NotesAppTests
+//
+//  Created by Abdel Azim Saleh on 29/07/2025.
+//
+
+import Testing
+@testable import NotesApp
+
+struct NotesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
